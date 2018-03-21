3fdb8d61576671bc24f2d3b2fa64039b359c479d
gautam678 <gautam678@gmail.com>
2018-03-21 16:04:10 -0400

    Rewrote preprocessing code to handle errors

--- codePython/preprocess_pres.py
+++ codePython/preprocess_pres.py
@@ -30,22 +30,31 @@ def main():
         data={}
         file_object  = open(filename, 'r')
         filename = filename.split('/')[-1]
-        data['index'] = str(count+1)
-        data['Filename'] = str(filename)
-        content = file_object.read()
-        content,data['DCPD'] = break_content(content,'DCPD Number:')
-        content,data['Subject'] = break_content(content,'Subjects:')
-        content,data['Names'] = break_content(content,'Names:')
-        content,data['Location'] = break_content(content,'Locations:')
-        content,data['Categories'] = break_content(content,'Categories')
-        content,data['Note'] = break_content(content,'NOTE:')
-        time = time_match(content)
-        data['Time Posted'] = str(time)
-        title,text = break_content(content,time)
-        data['Title'] = title.split('\n')[2]
-        data['text'] = text
-        final_data[count]=data
-        count+=1
+        blacklist = ['CHECKLIST','NOMINATIONS','DIGEST','ACTSAPPROVED']
+        if  any(re.findall(r'|'.join(blacklist), filename, re.IGNORECASE))==False:
+            data['index'] = str(count+1)
+            data['Filename'] = str(filename)
+            content = file_object.read()
+            content,data['DCPD'] = break_content(content,'DCPD Number:')
+            content,data['Subject'] = break_content(content,'Subjects:')
+            content,data['Names'] = break_content(content,'Names:')
+            content,data['Location'] = break_content(content,'Locations:')
+            content,data['Categories'] = break_content(content,'Categories')
+            content,data['Note'] = break_content(content,'NOTE:')
+            time = time_match(content)
+            data['Time Posted'] = str(time)
+            title,text = break_content(content,time)
+            if count == 128:
+                print (filename)
+                print (text)
+            data['Title'] = title.split('\n')[2]
+            text = text.replace(',', '')
+            text = text.replace('\n', '')
+            data['text'] = text
+            if count ==128:
+                print(data['text'])
+            final_data[count]=data
+            count+=1
     header = list(data.keys())
     with open('presDocument.csv', 'w+') as f:
         writer = csv.DictWriter(f, fieldnames=header)
